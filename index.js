9a4db510dc69ece33a5f3992c5f9fecc96e5ae30
supapronpunphet <supapronpunphet05@gmail.com>
2026-03-16 18:35:50 +0700

    fix vercel mysql pool

--- index.js
+++ index.js
@@ -1,100 +1,101 @@
-const express = require('express');
-const mysql = require('mysql2');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const mysql = require("mysql2/promise");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const connection = mysql.createConnection(process.env.DATABASE_URL);
+const pool = mysql.createPool({
+  uri: process.env.DATABASE_URL,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
 
 // test route
-app.get('/', (req, res) => {
-  res.send('API Working');
+app.get("/", (req, res) => {
+  res.send("API Working");
 });
 
 // GET all users
-app.get('/users', (req, res) => {
-  connection.query('SELECT * FROM users', (err, result) => {
-    if (err) {
-      res.status(500).json(err);
-    } else {
-      res.json(result);
-    }
-  });
+app.get("/users", async (req, res) => {
+  try {
+    const [rows] = await pool.query("SELECT * FROM users");
+    res.json(rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
 });
 
 // GET user by id
-app.get('/users/:id', (req, res) => {
+app.get("/users/:id", async (req, res) => {
   const id = req.params.id;
 
-  connection.query(
-    'SELECT * FROM users WHERE id = ?',
-    [id],
-    (err, result) => {
-      if (err) {
-        res.status(500).json(err);
-      } else {
-        res.json(result[0]);
-      }
-    }
-  );
+  try {
+    const [rows] = await pool.query(
+      "SELECT * FROM users WHERE id = ?",
+      [id]
+    );
+    res.json(rows[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
 });
 
 // POST create user
-app.post('/users', (req, res) => {
+app.post("/users", async (req, res) => {
   const { fname, lname, username, password, avatar } = req.body;
 
-  connection.query(
-    'INSERT INTO users (fname, lname, username, password, avatar) VALUES (?, ?, ?, ?, ?)',
-    [fname, lname, username, password, avatar],
-    (err, result) => {
-      if (err) {
-        res.status(500).json(err);
-      } else {
-        res.json(result);
-      }
-    }
-  );
+  try {
+    const [result] = await pool.query(
+      "INSERT INTO users (fname, lname, username, password, avatar) VALUES (?, ?, ?, ?, ?)",
+      [fname, lname, username, password, avatar]
+    );
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
 });
 
 // PUT update user
-app.put('/users/:id', (req, res) => {
+app.put("/users/:id", async (req, res) => {
   const id = req.params.id;
   const { fname, lname, username, password, avatar } = req.body;
 
-  connection.query(
-    'UPDATE users SET fname=?, lname=?, username=?, password=?, avatar=? WHERE id=?',
-    [fname, lname, username, password, avatar, id],
-    (err, result) => {
-      if (err) {
-        res.status(500).json(err);
-      } else {
-        res.json(result);
-      }
-    }
-  );
+  try {
+    const [result] = await pool.query(
+      "UPDATE users SET fname=?, lname=?, username=?, password=?, avatar=? WHERE id=?",
+      [fname, lname, username, password, avatar, id]
+    );
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
 });
 
 // DELETE user
-app.delete('/users/:id', (req, res) => {
+app.delete("/users/:id", async (req, res) => {
   const id = req.params.id;
 
-  connection.query(
-    'DELETE FROM users WHERE id = ?',
-    [id],
-    (err, result) => {
-      if (err) {
-        res.status(500).json(err);
-      } else {
-        res.json(result);
-      }
-    }
-  );
+  try {
+    const [result] = await pool.query(
+      "DELETE FROM users WHERE id = ?",
+      [id]
+    );
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
 });
 
 app.listen(3000, () => {
-  console.log('Server running on port 3000');
+  console.log("Server running on port 3000");
 });
